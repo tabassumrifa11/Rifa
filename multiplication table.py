0ec283fdcbb0b234da3a646baa6212a5b28f0536
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-04 23:09:27 +0600

    multiplication

---
+++ multiplication table.py
@@ -0,0 +1,15 @@
+print('\033c')
+
+num = int(input("Enter a number for the multiplication table: "))
+n = 1
+
+print(f"Multiplication table for {num}:")
+
+
+
+for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+        
+        
+    
+    
